74372aba427edbdeb45d7267fd3d4fc3cf385ca6
Josué <josue.santos@svm.com.br>
2021-03-15 14:24:20 -0300

    craft maker v.0.0.1

---
+++ assets/js/app.js
@@ -0,0 +1,134 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB89Q0zZoegIQ89li-FQ9wwwugyHng59p4",
+  authDomain: "projetocrud-abcad.firebaseapp.com",
+  projectId: "projetocrud-abcad",
+  storageBucket: "projetocrud-abcad.appspot.com",
+  messagingSenderId: "124181371095",
+  appId: "1:124181371095:web:0f447fababa039ea97f892",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const craft_user_logger = window.sessionStorage.getItem("craft_user_logger");
+if (craft_user_logger) {
+  document.querySelectorAll(".item-not-log").forEach(function (element) {
+    element.remove();
+  });
+} else {
+  document.querySelectorAll(".item-log").forEach(function (element) {
+    element.remove();
+  });
+}
+
+function salveCraft() {
+  const idUser = window.sessionStorage.getItem("craft_user_logger") || false;
+
+  const id = window.sessionStorage.getItem("craftArtsId") || false;
+  const cabelo = window.sessionStorage.getItem("cabelo");
+  const sombrancelha = window.sessionStorage.getItem("sombrancelha");
+  const olho = window.sessionStorage.getItem("olho");
+  const nariz = window.sessionStorage.getItem("nariz");
+  const boca = window.sessionStorage.getItem("boca");
+
+  if (idUser) {
+    if (id) {
+      db.collection("craftArts")
+        .doc(id)
+        .update({
+          idUser,
+          cabelo,
+          sombrancelha,
+          olho,
+          nariz,
+          boca,
+        })
+        .then(() => {
+          alert("Craft Editado com sucesso!");
+        });
+    } else {
+      db.collection("craftArts")
+        .add({
+          idUser,
+          cabelo,
+          sombrancelha,
+          olho,
+          nariz,
+          boca,
+        })
+        .then(() => {
+          alert("Craft Salvo com sucesso!");
+        });
+    }
+  } else {
+    alert("Efetue o login");
+  }
+}
+
+function saveDataRegister() {
+  const nome = document.querySelector("input#nome").value;
+  const email = document.querySelector("input#email").value;
+  const password = document.querySelector("input#password").value;
+
+  db.collection("craftUsers")
+    .add({
+      nome,
+      email,
+      password,
+    })
+    .then(() => {
+      alert("Usuário cadastrado com sucesso!");
+      location.href = "index.html";
+    });
+}
+
+function logDataUser() {
+  const email = document.querySelector("input#email").value;
+  const password = document.querySelector("input#password").value;
+
+  db.collection("craftUsers")
+    .where("email", "==", email)
+    .where("password", "==", password)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        window.sessionStorage.setItem("craft_user_logger", doc.id);
+        window.sessionStorage.setItem("craft_user_name", doc.data().nome);
+        alert("Seja Bem vindo!");
+        location.href = "index.html";
+      });
+    })
+    .catch((error) => {
+      Alert("Erro no login: ", error);
+    });
+}
+
+function getNameCraft() {
+  if (window.sessionStorage.getItem("craft_user_name")) {
+    return window.sessionStorage.getItem("craft_user_name");
+  }
+  return "";
+}
+
+function logout() {
+  window.sessionStorage.removeItem("craft_user_logger");
+  window.sessionStorage.removeItem("craft_user_name");
+
+  alert("Tchau até a próxima");
+  location.href = "index.html";
+}
+
+function deleteCraft(id) {
+  db.collection("craftArts")
+    .doc(id)
+    .delete()
+    .then(() => {
+      alert("Craft apagado com sucesso!");
+      location.href = "myCrafts.html";
+    })
+    .catch((error) => {
+      alert("Erro ao deletar: ", error);
+    });
+}
---
+++ assets/js/preview.js
@@ -0,0 +1,70 @@
+const elementsArray = [];
+
+elementsArray.cabelo = window.sessionStorage.getItem("cabelo");
+elementsArray.sombrancelha = window.sessionStorage.getItem("sombrancelha");
+elementsArray.olho = window.sessionStorage.getItem("olho");
+elementsArray.nariz = window.sessionStorage.getItem("nariz");
+elementsArray.boca = window.sessionStorage.getItem("boca");
+
+let html = dataCraftArray.block.preview.blockIn;
+if (elementsArray.cabelo != "false") {
+  html += dataCraftArray.element[elementsArray.cabelo].dataPreview;
+}
+if (elementsArray.sombrancelha != "false") {
+  html +=
+    dataCraftArray.block.preview.sombrancelha +
+    dataCraftArray.element[elementsArray.sombrancelha].data +
+    dataCraftArray.block.preview.blockOut;
+}
+if (elementsArray.olho != "false") {
+  html +=
+    dataCraftArray.block.preview.olho +
+    dataCraftArray.element[elementsArray.olho].data +
+    dataCraftArray.block.preview.blockOut;
+}
+if (elementsArray.nariz != "false") {
+  html +=
+    dataCraftArray.block.preview.nariz +
+    dataCraftArray.element[elementsArray.nariz].data +
+    dataCraftArray.block.preview.blockOut;
+}
+if (elementsArray.boca != "false") {
+  html +=
+    dataCraftArray.block.preview.boca +
+    dataCraftArray.element[elementsArray.boca].data +
+    dataCraftArray.block.preview.blockOut;
+}
+
+var data =
+  '<svg xmlns="http://www.w3.org/2000/svg" id="svg-craft" viewBox="0 0 203.2 152.4">' +
+  html +
+  "</svg>";
+const img_svg = document.getElementById("svg-craft");
+img_svg.src = "data:image/svg+xml;utf8," + encodeURIComponent(data);
+
+function editCraft() {
+  window.sessionStorage.setItem("cabelo", elementsArray.cabelo || false);
+  window.sessionStorage.setItem(
+    "sombrancelha",
+    elementsArray.sombrancelha || false
+  );
+  window.sessionStorage.setItem("olho", elementsArray.olho || false);
+  window.sessionStorage.setItem("nariz", elementsArray.nariz || false);
+  window.sessionStorage.setItem("boca", elementsArray.boca || false);
+
+  window.sessionStorage.setItem("pageEdit", true);
+
+  location.href = "creativity.html";
+}
+
+function download_img(el) {
+  const canva = document.createElement("canvas");
+  canva.width = 203.2;
+  canva.height = 152.4;
+  const context_canva = canva.getContext("2d");
+
+  context_canva.drawImage(img_svg, 0, 0, canva.width, canva.height);
+
+  var image = canva.toDataURL("image/jpg");
+  el.href = image;
+}
